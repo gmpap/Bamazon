eb399823629a7463624e9c6cfd27208fc5b76141
George <dog8dog1@live.com>
2019-10-17 11:11:08 -0400

    finished cli and update orders

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -65,11 +65,46 @@ let shop = function() {
             shop();
           } else {
             //use inquirer to get our quantity
-            inquirer.prompt({
-              name: "quantity",
-              type: "input",
-              message: "How many units would you like to buy.?"
-            });
+            inquirer
+              .prompt({
+                name: "quantity",
+                type: "input",
+                message: "How many units would you like to buy.?"
+              })
+              .then(function(answer2) {
+                let quantity = answer2.stock_quantity;
+                if (quantity > res[0].stock_quantity) {
+                  console.log(
+                    "We are sorry, we only have " +
+                      res[0].stock_quantity +
+                      "items in stock"
+                  );
+
+                  shop();
+                } else {
+                  //outputs the item selected
+                  console.log("");
+                  console.log(res[0].product_name + "Purchased");
+                  console.log(quantity + " qty @ $ " + res[0].price);
+
+                  //This updates the quantity in the db
+                  let newQuantity = res[0].stock_quantity - quantity;
+                  connection.query(
+                    "UPDATE products SET stock_quantity = " +
+                      newQuantity +
+                      " WHERE id = " +
+                      res[0].item_id,
+                    function(err, resUpdate) {
+                      if (err) throw err;
+                      console.log("");
+                      console.log("Your Order has been submitted.");
+                      console.log("Thank you for buying from us.!");
+                      console.log("");
+                      connection.end();
+                    }
+                  );
+                }
+              });
           }
         }
       );
