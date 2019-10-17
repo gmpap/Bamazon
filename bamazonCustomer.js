eaeacadf0c05760891f8975f7ed6c9948b3225e4
George <dog8dog1@live.com>
2019-10-17 10:46:18 -0400

    created cli inputs

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,23 +1,27 @@
 //Author: George Pap
 let mysql = require("mysql");
 let inquirer = require("inquirer");
+let Table = require("cli-table");
 
-//Connect to my database
+//Connect to mysql database
 let connection = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "",
-  database: "Bamazon"
-  //port: 3306
+  password: "secret",
+  database: "bamazon",
+  port: 3306
 });
 
-//Connecting to the Bamazon DB
-connection.connect();
+//Connecting to the Bamazon DB and test if the connection works
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("connection successful");
+  headerDisplay();
+});
 
 //display a Header row
 let headerDisplay = function() {
-  connection.query("SELECT * FROM products", 
-   {
+  connection.query("SELECT * FROM products", function(err, res) {
     if (err) throw err;
     console.log("    Welcome to Bamazon   ");
     console.log("----------------------------");
@@ -33,6 +37,43 @@ let headerDisplay = function() {
   }
   //console logs the table for viewing
   console.log(table.toString());
+};
+
+let shop = function() {
+  inquirer
+    .prompt({
+      name: "productToPurchase",
+      type: "input",
+      message:
+        "Please Select the Product Id of the item that you would like to buy.!"
+    })
+    .then(function(answer1) {
+      let choice = answer1.productToPurchase;
+      //connect to db
+      connection.query(
+        "SELCT * FROM products WHERE item_id=?",
+        selection,
+        function(err, res) {
+          if (err) throw err;
+          //This gives a error if the item selected doesnt exist
+          if (res.length === 0) {
+            console.log(
+              "That item does not exsist.  Please choice one from the list."
+            );
+
+            //call
+            shop();
+          } else {
+            //use inquirer to get our quantity
+            inquirer.prompt({
+              name: "quantity",
+              type: "input",
+              message: "How many units would you like to buy.?"
+            });
+          }
+        }
+      );
+    });
 };
 
 headerDisplay();
